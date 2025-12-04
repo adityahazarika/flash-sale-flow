d80aefe4ca320586e706c9596998fc4ded1f5604
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2025-12-04 15:21:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(3000, () => {
-  console.log("✅ Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
